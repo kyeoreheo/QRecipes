3702152033a470d17d3a42de2c817a12444bd698
kyeoreheo <91kyoheo@gmail.com>
2020-09-08 19:05:45 -0400

    FirstCommit

---
+++ QRecipes/QRecipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRecipes
+//
+//  Created by Kyo on 9/8/20.
+//  Copyright © 2020 Kyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
