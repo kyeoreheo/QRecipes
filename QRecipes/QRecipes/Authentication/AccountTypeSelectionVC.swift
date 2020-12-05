315e3db273783de696d195f44d61cb88b8b31f6a
Mingu Choi <32556382+MinguChoi@users.noreply.github.com>
2020-12-05 21:57:54 +0900

    added selectionVC

---
+++ QRecipes/QRecipes/Authentication/AccountTypeSelectionVC.swift
@@ -0,0 +1,15 @@
+//
+//  AccountTypeSelectionVC.swift
+//  QRecipes
+//
+//  Created by Mingu Choi on 2020/12/05.
+//  Copyright © 2020 Kyo. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class AccountTypeSelectionVC: UIViewController {
+    
+    
+}
