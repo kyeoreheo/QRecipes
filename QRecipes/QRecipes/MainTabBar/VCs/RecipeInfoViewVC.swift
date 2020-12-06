e2c4a6c405466d058327def62cb20664858c0a76
Dannyyny <61069233+Dannyyny@users.noreply.github.com>
2020-12-06 16:59:56 -0500

    removeUnnecessaryCode

--- QRecipes/QRecipes/MainTabBar/VCs/RecipeInfoViewVC.swift
+++ QRecipes/QRecipes/MainTabBar/VCs/RecipeInfoViewVC.swift
@@ -211,54 +211,6 @@ class RecipeInfoViewVC: UIViewController {
         tv.register(CommentCell.self, forCellReuseIdentifier: "cell")
         return tv
     } ()
-    /*
-    var profileImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.image = #imageLiteral(resourceName: "profile")
-        iv.contentMode = .scaleAspectFill
-        iv.clipsToBounds = true
-        iv.layer.borderWidth = 3
-        iv.layer.cornerRadius = 25
-        iv.translatesAutoresizingMaskIntoConstraints = false
-        iv.layer.borderColor = UIColor.white.cgColor
-        return iv
-    }()
-    lazy var userNameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Danny"
-        label.textColor = .gray
-        label.font = UIFont.systemFont(ofSize: 13 * ratio, weight: UIFont.Weight.bold)
-        
-        return label
-    }()
-    lazy var commentLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Love it, it is easy to follow.Thank you. :)"
-        label.textColor = .black
-        label.font = UIFont(name:"Helvetica", size: 14 * ratio)
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        label.sizeToFit()
-        
-        return label
-    }()
-    lazy var thumbsUpButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.tintColor = .primeOrange
-        button.backgroundColor = .white
-        button.setImage(UIImage(systemName: "hand.thumbsup"), for: .normal)
-        
-        return button
-    }()
-    lazy var thumbsDownButton: UIButton = {
-        let button = UIButton(type: .custom)
-        button.tintColor = .primeOrange
-        button.backgroundColor = .white
-        button.setImage(UIImage(systemName: "hand.thumbsdown"), for: .normal)
-        
-        return button
-    }()
- */
     
     private let isInPurchaseFlow: Bool
     
@@ -467,40 +419,6 @@ class RecipeInfoViewVC: UIViewController {
             make.bottom.equalToSuperview().offset(-15)
         }
  
-        /*
-        tableView.addSubview(profileImageView)
-        profileImageView.snp.makeConstraints { make in
-            make.width.height.equalTo(50 * ratio)
-            make.top.left.equalToSuperview().offset(15)
-            //make.size.equalTo(view.frame.width-320)
-            make.left.equalToSuperview().offset(15)
-        }
-        tableView.addSubview(userNameLabel)
-        userNameLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(20)
-            make.left.equalTo(profileImageView.snp.right).offset(30)
-            
-        }
-        tableView.addSubview(commentLabel)
-        commentLabel.snp.makeConstraints { make in
-            make.top.equalTo(userNameLabel).offset(25)
-            make.left.equalTo(profileImageView.snp.right).offset(30)
-            make.right.equalTo(commentView).offset(-20)
-        }
-        
-        tableView.addSubview(thumbsUpButton)
-        thumbsUpButton.snp.makeConstraints { make in
-            make.top.equalTo(userNameLabel).offset(70)
-            make.right.equalTo(commentView).offset(-50)
-        }
-        
-        tableView.addSubview(thumbsDownButton)
-        thumbsDownButton.snp.makeConstraints { make in
-            make.top.equalTo(userNameLabel).offset(70)
-            make.right.equalTo(commentView).offset(-20)
-        }
-    
-    */
     }
     private func fetchRestaurant() {
         guard let recipe = recipe else { return }
